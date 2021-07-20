222dee80736f7b6bdb407db2412a351301cff466
Zhivaev Artem <zhivaev993@gmail.com>
2021-07-20 20:13:26 +0300

    ...

--- pets_2/src/main/java/com/example/pets/config/RootConfig.java
+++ pets_2/src/main/java/com/example/pets/config/RootConfig.java
@@ -41,6 +41,7 @@ public class RootConfig {
         factoryBean.setDataSource(dataSource());
         Properties props = new Properties();
         props.setProperty("hibernate.show_sql", "true");
+        props.setProperty("hibernate.hbm2ddl.auto", "create-drop");
         factoryBean.setHibernateProperties(props);
         return factoryBean;
     }
